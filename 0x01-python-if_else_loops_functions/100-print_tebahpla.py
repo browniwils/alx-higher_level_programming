73d76393cc9f7d3bffb1939ae9d6f8100ef75fae
Browni Wilson <browniwils@gmail.com>
2023-05-10 15:34:44 +0000

    added tasks for 0x01 advance

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
-for char in reversed(range(97, 123)):
-    print("{:c}".format(char % 2 == 0) else (char - 32)), end="")
+i = 0
+for char in range(122, 96, -1):
+    print("{}".format(chr(char - i), end="")
+    if i == 0:
+        i = 32
+    else:
+        i = 0
