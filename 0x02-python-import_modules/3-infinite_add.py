2ad3a9368936ab653a2f884f0a1332c333cf46ec
Browni Wilson <browniwils@gmail.com>
2023-05-12 00:21:00 +0000

    added task 3 for 0x02

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import sys
     sum = 0
     for n in range(len(sys.argv) - 1):
-        sum =  sum + int(sys.argv[n + 1])
+        sum = sum + int(sys.argv[n + 1])
     print("{}".format(sum))
