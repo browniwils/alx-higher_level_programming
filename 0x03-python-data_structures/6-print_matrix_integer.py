b6d6402f6d634cb08ad971a0e756d84cfaee73b9
Browni Wilson <browniwils@gmail.com>
2023-05-15 18:02:14 +0000

    added task 6 for 0x03

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,8 @@ def print_matrix_integer(matrix=[[]]):
     """ Print integer in multi dimentional list """
     for mat in matrix:
         for m in mat:
-            print("{:d}".format(m), end="")
+            if mat.index(m) == len(mat) - 1:
+                print("{:d}".format(m), end="")
+            else:
+                print("{:d}".format(m), end=" ")
         print()
