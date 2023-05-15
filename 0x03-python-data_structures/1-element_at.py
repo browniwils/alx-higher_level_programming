1428bf727bb31e89b02e0175f454b52060e5d4b5
Browni Wilson <browniwils@gmail.com>
2023-05-15 14:52:41 +0000

    added task 1 for 0x03

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,7 +2,9 @@
 
 def element_at(my_list, idx):
     """ Retreive and returns element at index idx """
-    if idx < 0 or idx > len(my_list):
+    if idx < 0:
+        return (None)
+    elif idx > len(my_list):
         return (None)
     else:
         return (my_list[idx])
