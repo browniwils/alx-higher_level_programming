bd1f2fb89b50fb7625f4bcd05520e851ca58b021
Browni Wilson <browniwils@gmail.com>
2023-05-15 14:37:04 +0000

    added task 0 for 0x03

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,5 +3,4 @@
 def print_list_integer(my_list=[]):
     """ Prints all integers of a list """
     for i in my_list:
-        if type(i) == int:
-            print("{:d}".format(i))
+        print("{:d}".format(i))
