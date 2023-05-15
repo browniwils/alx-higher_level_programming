cd3f8ca0bd6f23ae2ade86622afc06e519d71097
Browni Wilson <browniwils@gmail.com>
2023-05-15 17:07:53 +0000

    added task 4 for 0x03

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,9 +2,7 @@
 
 def new_in_list(my_list, idx, element):
     """ Replace item in a list with element at idx and return new list """
-    if idx < 0:
-        return my_list
-    elif idx > len(my_list) - 1:
+    if idx < 0 or idx > len(my_list) - 1:
         return [i for i in my_list]
     else:
         new_list = []
