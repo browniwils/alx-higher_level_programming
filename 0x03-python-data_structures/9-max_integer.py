716b57763d9ce8f6c3c993743f6aaab8063a9b61
Browni Wilson <browniwils@gmail.com>
2023-05-16 21:58:22 +0000

    added task 9 for 0x03

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,8 +4,10 @@ def max_integer(my_list=[]):
     """ Finds maximum integer in list """
     if len(my_list) < 1:
         return None
-    max_int = 0
-    for i in range(len(my_list)):
+
+    max_int = my_list[0]
+    for i in range(1, len(my_list)):
         if my_list[i] > max_int:
             max_int = my_list[i]
+
     return max_int
