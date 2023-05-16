de3e360af346f85b5c5bd80fc67f1ec1722ffad5
Browni Wilson <browniwils@gmail.com>
2023-05-16 22:08:52 +0000

    added task 10 for 0x03

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
 
 def divisible_by_2(my_list=[]):
-    my_list_bool = []
-    for i in range(len(my_list)):
-        if my_list[i] % 2 == 0:
-            my_list_bool[i] = True
-        else:
-            my_list_bool[i] = False
-
-    return my_list_bool
+    return [True if i % 2 == 0 else False for i in my_list]
