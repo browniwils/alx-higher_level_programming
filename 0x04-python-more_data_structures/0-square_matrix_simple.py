0a7216cad0e7bec98cf86fc1b69140350d9fcc3b
Browni Wilson <browniwils@gmail.com>
2023-05-17 17:45:13 +0000

    added task 0 for 0x04

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,6 +3,6 @@
 def square_matrix_simple(matrix=[]):
     n_matrix = []
     for i in matrix:
-	n_matrix.append(list(map(lambda x: x * x, i)))
+        n_matrix.append(list(map(lambda x: x * x, i)))
 
     return n_matrix
