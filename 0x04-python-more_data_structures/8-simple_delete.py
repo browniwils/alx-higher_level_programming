b6fc76c9c1bed0f2f2bce7fc48d285c1feb3a283
Browni Wilson <browniwils@gmail.com>
2023-05-17 23:22:03 +0000

    added task 8 for 0x04

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -4,7 +4,7 @@ def simple_delete(a_dictionary, key=""):
     if not key or not a_dictionary:
         return a_dictionary
 
-    if a_dictionary[key] is not None
+    if a_dictionary[key] is not None:
         del a_dictionary[key]
 
     return a_dictionary
