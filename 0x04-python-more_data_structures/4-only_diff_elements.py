f4a3b5a81297c5ff96a1e946e1efe7ce227cff4d
Browni Wilson <browniwils@gmail.com>
2023-05-17 18:32:07 +0000

    added task 4 for 0x04

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
 def only_diff_elements(set_1, set_2):
-    len_1 = len(set_1)
-    len_2 = len(set_2)
-    if len_2 > len_1:
-        return [i for i in set_2 if i not in set_1]
-    else:
-        return [i for i in set_1 if i not in set_2]
+    diff = []
+    for i in set_1:
+        if i not in set_2:
+            diff.append(i)
+    for i in set_2:
+        if i not in set_1:
+            diff.append(i)
+    return diff
