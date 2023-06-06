4c42c4305156875768093e46b84ef46ddc81a8bd
Browni Wilson <browniwils@gmail.com>
2023-06-06 12:32:22 +0000

    update task 6

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -8,6 +8,7 @@ class Rectangle:
     """ A class for a rectangle """
 
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         type(self).number_of_instances += 1
         self.width = width
