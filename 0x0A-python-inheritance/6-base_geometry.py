0015c1effa5d92b803c87596568735d87d2933ec
Browni Wilson <browniwils@gmail.com>
2023-06-13 14:31:18 +0000

    added task 6 for 0x0A

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module defines base geometry class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+
+    def area(self):
+        """No implementatio yet"""
+        raise Exception("area() is not implemented")
