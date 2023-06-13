6c4cfc67f54dd53f428f186f0464946a5bf6ed73
Browni Wilson <browniwils@gmail.com>
2023-06-13 14:14:24 +0000

    added task 0 for 0x0A

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines an object attribute lookup function
+"""
+
+
+def lookup(obj):
+    """Return a list of object's available attributes or fields
+    """
+    return dir(obj)
