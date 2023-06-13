40ee2d802cca700d40619da7c6e310a47e536ff3
Browni Wilson <browniwils@gmail.com>
2023-06-13 15:50:41 +0000

    updated task 11

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """Module defines a Rectangle subclass Square
 """
 Rectangle = __import__('9-rectangle').Rectangle
@@ -7,11 +8,9 @@ class Square(Rectangle):
     """Represent a square."""
 
     def __init__(self, size):
-        """Initialize a new square.
-
-        Args:
-            size (int): The size of the new square.
-        """
-        self.integer_validator("size", size)
-        super().__init__(size, size)
+        """ size init"""
         self.__size = size
+        super().__init__(self.__size, self.__size)
+
+    def __str__(self):
+        return ("[Square] " + str(self.__size) + "/" + str(self.__size))
