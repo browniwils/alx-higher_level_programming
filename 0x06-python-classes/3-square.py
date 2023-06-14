0da6bc245bcad9627668820b5e901e558395f4bf
Browni Wilson <browniwils@gmail.com>
2023-06-14 18:32:46 +0000

    added task 3 for 0x06

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""Module defines a class for square
+"""
+
+
+class Square:
+    """Class represent a square object"""
+
+    def __init__(self, size=0):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Return the current area of the square."""
+        return self.__size * self.__size
