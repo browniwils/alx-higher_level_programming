9fb2f5335dd3490b2fc00e76dcdbd44f89cfc6a8
Browni Wilson <browniwils@gmail.com>
2023-06-14 18:31:19 +0000

    added task 1 for 0x06

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""Module defines a class for square
+"""
+
+
+class Square:
+    """Class represent a square object
+    """
+
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
