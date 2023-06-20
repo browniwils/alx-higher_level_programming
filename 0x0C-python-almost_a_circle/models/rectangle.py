7e18d6030616bd65285f740c206de1a087ff515e
Browni Wilson <browniwils@gmail.com>
2023-06-20 14:06:02 +0000

    added task 3, 4, 5, 6, 7, 8, and 9 for 0x0C

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,7 +4,7 @@ from models.base import Base
 
 
 class Rectangle(Base):
-    """Class represent a rectangle."""
+    """Class represent a rectangle which inherits parents properties"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
         self.width = width
