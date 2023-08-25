414c774c635fe6c798ee8d13e45aa08c7877bafe
Browni Wilson <browniwils@gmail.com>
2023-08-25 20:27:15 +0000

    added task 3 for 0x0F

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Program lists all states from database
+"""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    args = sys.argv
+    host = "localhost" 
+    passcode = args[2]
+    user = args[1]
+    database = args[3]
+    port = 3306
+
+    db = MySQLdb.connect(host=host,
+                         port=port, passwd=passcode,
+                         db=database, user=user)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE %s", (args[4], ))
+    row_data = cursor.fetchall()
+    for data in row_data:
+        print(data)
+    cursor.close()
+    db.close()
