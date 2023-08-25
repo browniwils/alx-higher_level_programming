10229996a669e09a54771557a1bab44123f83a7d
Browni Wilson <browniwils@gmail.com>
2023-08-25 20:34:22 +0000

    added advance task 102 for 0x0F

---
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Connect model class to table in database
+"""
+import sys
+from relationship_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    args = sys.argv
+    host = "localhost" 
+    passcode = args[2]
+    user = args[1]
+    database = args[3]
+    port = 3306
+
+    engine_config = 'mysql+mysqldb://{}:{}@{}:{}/{}'.format(user, passcode, host, port, database)
+    engine = create_engine(engine_config, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+
+    data = Session.query(State).order_by(State.id)
+    for row in data:
+        for city in row.cities:
+            print(city.id, city.name, sep=": ", end="")
+            print(" -> {}".format(row.name))
