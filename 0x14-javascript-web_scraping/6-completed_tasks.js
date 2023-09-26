e0607f5fbc0b51d01729d032e821f933bf58e1ed
Browni Wilson <browniwils@gmail.com>
2023-09-26 19:36:47 +0000

    added task 6 for 0x14

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const request = require("request");
+
+request(process.argv[2], function (err, res, body) {
+  if (!err) {
+    const data = JSON.parse(body);
+    const users = {};
+
+    data.forEach((element) => {
+      if (element.completed && users[element.userId] === undefined) {
+        users[element.userId] = 1;
+      } else if (element.completed) {
+        users[element.userId] += 1;
+      }
+    });
+    console.log(users);
+  }
+});
