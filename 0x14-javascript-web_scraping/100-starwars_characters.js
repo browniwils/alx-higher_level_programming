236eaaae7f6e299937f6d4e9fe0ac8bcac542c34
Browni Wilson <browniwils@gmail.com>
2023-09-26 19:37:03 +0000

    added advance task 100 for 0x14

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+
+const request = require("request");
+const id = process.argv[2];
+const base = "https://swapi-api.alx-tools.com/api/films/";
+const url = base.concat(id);
+
+request(url, (error, res, body) => {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    let charactersProcessed = 0;
+    const characterNames = [];
+
+    characters.forEach((characterUrl) => {
+      request(characterUrl, (error, res, body) => {
+        if (!error) {
+          const char = JSON.parse(body).name;
+          characterNames.push(char);
+        }
+        charactersProcessed++;
+        if (charactersProcessed === characters.length) {
+          characterNames.forEach((actor) => {
+            console.log(actor);
+          });
+        }
+      });
+    });
+  } else {
+    console.log(error);
+  }
+});
