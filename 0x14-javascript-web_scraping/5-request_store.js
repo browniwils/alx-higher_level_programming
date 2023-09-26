4bedfe82f0c605ec8d8375534c4f5b679b330c74
Browni Wilson <browniwils@gmail.com>
2023-09-26 19:36:37 +0000

    added task 5 for 0x14

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const fs = require("fs");
+const request = require("request");
+const url = process.argv[2];
+const name = process.argv[3];
+
+request(url, (error, res, body) => {
+  if (error) {
+    console.error(error);
+  }
+
+  fs.writeFile(name, body, "utf-8", (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+});
