460b3154fe18b2532ed2727d487ec7447eda9fe2
Browni Wilson <browniwils@gmail.com>
2023-09-27 13:35:34 +0000

    updated task 101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -7,18 +7,15 @@ const url = base.concat(id);
 
 request(url, function (error, res, body) {
   if (!error) {
-    const characters = JSON.parse(body).characters;
+    const { characters } = JSON.parse(body).results;
+    characters.forEach((element) => {
+      request(element, function (error, res, body) {
+        if (!error) {
+          const { name } = JSON.parse(body);
+          console.log(name);
+        }
+      });
+    });
     printChars(characters, 0);
   }
 });
-
-function printChars(characters, index) {
-  request(characters[index], function (error, res, body) {
-    if (!error) {
-      console.log(JSON.parse(body).name);
-      if (index + 1 < characters.length) {
-        printChars(characters, index + 1);
-      }
-    }
-  });
-}
